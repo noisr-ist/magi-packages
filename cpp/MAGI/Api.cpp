538c1287e68ab24402fec33396e9e27abafef261
Duarte Gonçalves <duarte.d.goncalves@tecnico.ulisboa.pt>
2024-05-02 13:42:48 +0100

    Corrected get numbers bug

--- cpp/MAGI/Api.cpp
+++ cpp/MAGI/Api.cpp
@@ -7,7 +7,7 @@ Api::Api() {}
 
 json Api::generate(int count) {
     cpr::Response r = cpr::Post(cpr::Url{"https://api.noisr.pt/generate"},
-                                 cpr::Body("{\"count\": 5}"),
+                                 cpr::Body("{\"count\": " + std::to_string(count) + "}"),
                                  cpr::Header{{"Content-Type", "application/json"}});
 
     json j = nlohmann::json::parse(r.text);
